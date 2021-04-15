f5960b413691ea0b0861b71f676c4aa2bb6516ad
umasikkal <umasikkalkumar@gmail.com>
2021-04-14 22:28:28 -0500

    commit

--- mission-planning-dashboard/src/app/app.module.ts
+++ mission-planning-dashboard/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CrewComponent } from './crew/crew.component';
+import { CssComponent } from './css/css.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CrewComponent,
+    CssComponent
   ],
   imports: [
     BrowserModule
